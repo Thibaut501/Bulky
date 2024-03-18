01ae0cf58cedeac900977aac981c0a006fb75e74
Thibaut Fabrice <thibautfabjoey23@gmail.com>
2024-03-18 17:54:16 +0400

    Fab007

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>(); 
 
 var app = builder.Build();
