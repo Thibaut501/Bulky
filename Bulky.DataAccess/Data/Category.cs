935e1e33b62fe7e741dde984de78e15d4b74c099
Thibaut Fabrice <thibautfabjoey23@gmail.com>
2024-03-20 08:13:07 +0400

    Fab0059

--- Bulky.DataAccess/Data/Category.cs
+++ Bulky.DataAccess/Data/Category.cs
@@ -1,6 +1,13 @@
-﻿namespace Bulky.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
 {
 	public class Category
 	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		public int DisplayOrder { get; set; }
 	}
 }
